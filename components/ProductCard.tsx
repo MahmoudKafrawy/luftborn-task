634b646875e18703016827d754e50e021f31de6d
MahmoudElkafrawy-gebhaly <mahmoud.kafrawy@egparcelexpress.com>
2023-02-03 10:02:56 +0200

    fix

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -13,12 +13,12 @@ export interface ProductDetails {
 }
 
 const ProductCard: React.FC<{ productDetails: ProductDetails }> = ({ productDetails }) => {
-  const months = ["#ff8b64", "#55c2e6", "#ff5e7d", "#7335d2", "#4bcf82", "#f1c75b"];
+  const colors = ["#ff8b64", "#55c2e6", "#ff5e7d", "#7335d2", "#4bcf82", "#f1c75b"];
 
-  const random = Math.floor(Math.random() * months.length);
+  const random = Math.floor(Math.random() * colors.length);
 
   return (
-    <div className={styles.container} style={{ backgroundColor: months[random] }}>
+    <div className={styles.container} style={{ backgroundColor: colors[random] }}>
       <div className={styles.card}>
         <div className={styles.options}>
           <div className={styles.title}>{textShrink(productDetails?.category, 20)}</div>
